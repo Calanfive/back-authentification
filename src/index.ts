2fb78d3b0ea14da79de220cdd1bf5d8fef71b238
calanfive <camille.lelan@gmail.com>
2023-12-06 09:30:32 +0100

    install sqlite3 + readme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import express from "express";
 import 'dotenv/config';
 import cors from "cors";
+import bodyParser from "body-parser";
+import { DataTypes, Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db.sqlite",
+})
+
+const User = sequelize.define("users", {
+    id: {
+      type: DataTypes.NUMBER,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  })
+
+sequelize.sync();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
+const port = process.env.PORT ? parseInt(process.env.PORT as string) : 1992
 
 app.get('/random-between/:min/:max', (req, res) => {
     const min = parseInt(req.params.min)
@@ -18,3 +40,4 @@ app.get('/random-between/:min/:max', (req, res) => {
 app.listen(port, () => {
     console.log('serveur running on port : ' + port);
 })
+

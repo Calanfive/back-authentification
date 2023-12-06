4f92f41cfe37a83aa4a45779a2d97e13de27fff0
calanfive <camille.lelan@gmail.com>
2023-12-06 10:49:16 +0100

    routes step 1

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const FreeGame = sequelize.define("free-games", {
     }
   })
 
-const OfficialGames = sequelize.define("official-games", {
+const OfficialGame = sequelize.define("official-games", {
     nom: {
         type: DataTypes.STRING,
       },
@@ -62,13 +62,70 @@ const port = process.env.PORT ? parseInt(process.env.PORT as string) : 1992
 // const myPlaintextPassword = 's0/\/\P4$$w0rD';
 // const hash = await bcrypt.hash(myPlaintextPassword, saltRounds);
 
-app.get('/random-between/:min/:max', (req, res) => {
-    const min = parseInt(req.params.min)
-    const max = parseInt(req.params.max)
-    const random = Math.floor(Math.random() * (max - min + 1)) + min
-    console.log('number' + random);
-    res.send(random.toString())
+app.post('/api/auth/local/register', (req, res) => {
+    res.send('Utilisateur créé')
 })
+
+app.post('/api/auth/local', (req, res) => {
+    res.send('Connection en cours')
+})
+
+app.get('/api/users/me', (req, res) => {
+    res.send('JWT reçu')
+})
+
+app.get('/api/auth/change-password', (req, res) => {
+    res.send('mot de passe modifié')
+})
+
+app.post('/api/free-games', (req, res) => {
+    res.send('nouveau jeu gratuit ajouté')
+})
+
+app.get('/api/free-games', (req, res) => {
+    res.send('jeux gratuits')
+})
+
+app.get('/api/free-games/:id', (req, res) => {
+    res.send('sélection jeu gratuit par id')
+})
+
+app.put('/api/free-games/:id', (req, res) => {
+    res.send('modif jeu gratuit après sélection par id')
+})
+
+app.delete('/api/free-games/:id', (req, res) => {
+    res.send('supression jeu gratuit après sélection par id')
+})
+
+app.post('/api/official-games', (req, res) => {
+    res.send('nouveau jeu payant ajouté')
+})
+
+app.get('/api/official-games', (req, res) => {
+    res.send('jeux payants')
+})
+
+app.get('/api/official-games/:id', (req, res) => {
+    res.send('sélection jeu payant par id')
+})
+
+
+app.put('/api/official-games/:id', (req, res) => {
+    res.send('modif jeu payant après sélection par id')
+})
+
+app.delete('/api/official-games/:id', (req, res) => {
+    res.send('supression jeu payant après sélection par id')
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('serveur running on port : ' + port);
